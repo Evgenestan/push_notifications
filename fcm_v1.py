3c96868d6e5f79aae3dd97d5f91e58737f366f4d
Evgenestan <evgenestan@yandex-team.ru>
2024-07-01 14:04:18 +0200

    added python script for sending push messages via FCM SDK

---
+++ fcm_v1.py
@@ -0,0 +1,31 @@
+import firebase_admin
+import datetime
+
+from sys import argv
+from firebase_admin import messaging
+
+# Don't forget to set GOOGLE_APPLICATION_CREDENTIALS
+# https://firebase.google.com/docs/cloud-messaging/auth-server#provide-credentials-manually
+
+script, registration_token, title, body = argv
+
+default_app = firebase_admin.initialize_app()
+message = messaging.Message(
+        notification=messaging.Notification(
+                title=title,
+                body=body,
+            ),
+        android=messaging.AndroidConfig(
+            ttl=datetime.timedelta(seconds=3600),
+            priority='normal',
+            notification=messaging.AndroidNotification(
+                title=title,
+                body=body,
+#                 image='https://foo.bar.pizza-monster.png'
+            ),
+        ),
+        token=registration_token
+    )
+
+response = messaging.send(message)
+print('Successfully sent message:', response)
